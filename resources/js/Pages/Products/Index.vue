9b18b14405a54f21b66e6145f274c7018bbb48a7
aridsonf <filhoarison@gmail.com>
2022-02-06 22:23:40 -0300

    style(List.vue | Index.vue)

--- resources/js/Pages/Products/Index.vue
+++ resources/js/Pages/Products/Index.vue
@@ -1,37 +1,34 @@
 <template>
-    <app-layout title="Dashboard">
-        <template #header>
-            <h2 class="font-semibold flex text-xl text-gray-800 leading-tight">
-                <CollectionIcon class="h-5 w-5 text-dark-500"/>&nbsp; 
-                Products
-            </h2>
-        </template>
+  <app-layout title="Dashboard">
+    <template #header>
+      <h2 class="font-semibold flex text-xl text-gray-800 leading-tight">
+        <CollectionIcon class="h-5 w-5 text-dark-500" />&nbsp; Products
+      </h2>
+    </template>
 
-        <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                    <list :products="products" />
-                </div>
-            </div>
+    <div class="py-12">
+      <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+          <list :products="products" />
         </div>
-    </app-layout>
+      </div>
+    </div>
+  </app-layout>
 </template>
 
 <script>
-    import { defineComponent } from 'vue'
-    import AppLayout from '@/Layouts/AppLayout.vue'
-    import List from '@/Pages/Products/List.vue'
-    import { 
-        CollectionIcon 
-        } from '@heroicons/vue/solid'
+import { defineComponent } from "vue";
+import AppLayout from "@/Layouts/AppLayout.vue";
+import List from "@/Pages/Products/List.vue";
+import { CollectionIcon } from "@heroicons/vue/solid";
 
-    export default defineComponent({
-        components: {
-            AppLayout,
-            List,
-            CollectionIcon,
-        },
+export default defineComponent({
+  components: {
+    AppLayout,
+    List,
+    CollectionIcon,
+  },
 
-        props: ['products'],
-    })
+  props: ["products"],
+});
 </script>
--- resources/js/Pages/Products/List.vue
+++ resources/js/Pages/Products/List.vue
@@ -1,79 +1,87 @@
 <template>
-    <div>
-        <div class="p-6 sm:px-20 bg-white border-b border-gray-200">
-            <div class="mt-8 text-2xl flex justify-between">
-                <div class="w-full h-full">
-                    List of products
-                </div>
+  <div>
+    <div class="p-6 sm:px-20 bg-white border-b border-gray-200">
+      <div class="mt-8 text-2xl flex justify-between">
+        <div class="w-full h-full">List of products</div>
 
-                <div class="">
-                    <button @click="modal()" class="bg-green-500 hover:bg-green-400 text-white font-bold py-2 px-4 border-b-4 border-green-700 hover:border-green-500 rounded-full">
-                        <PlusIcon class="h-5 w-5 text-dark-500"/>
-                    </button> 
-                </div>
-            </div>
+        <div class="">
+          <button
+            @click="modal()"
+            class="
+              bg-green-500
+              hover:bg-green-400
+              text-white
+              font-bold
+              py-2
+              px-4
+              border-b-4 border-green-700
+              hover:border-green-500
+              rounded-full
+            "
+          >
+            <PlusIcon class="h-5 w-5 text-dark-500" />
+          </button>
         </div>
-
-        <div class="bg-gray-200 bg-opacity-25 grid grid-cols-1">
-            <table class="table-auto">
-                <thead>
-                    <tr class="text-left">
-                        <th class="w-1/8 px-4 py-2">#</th>
-                        <th class="w-1/4 px-4 py-2">Name</th>
-                        <th class="w-1/2 px-4 py-2">Description</th>
-                        <th class="w-1/4 px-4 py-2"></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr v-for="product in products" :key="product.id">
-                        <td class="border px-4 py-2">{{product.id}}</td>
-                        <td class="border px-4 py-2">{{product.name}}</td>
-                        <td class="border px-4 py-2">{{product.description}}</td>
-                        <td class="border px-4 py-2"></td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
+      </div>
     </div>
 
-    <Create :showModal="showModal" ></Create>
+    <div class="bg-gray-200 bg-opacity-25 grid grid-cols-1">
+      <table class="table-auto">
+        <thead>
+          <tr class="text-left">
+            <th class="w-1/8 px-4 py-2">#</th>
+            <th class="w-1/4 px-4 py-2">Name</th>
+            <th class="w-1/2 px-4 py-2">Description</th>
+            <th class="w-1/4 px-4 py-2"></th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="product in products" :key="product.id">
+            <td class="border px-4 py-2">{{ product.id }}</td>
+            <td class="border px-4 py-2">{{ product.name }}</td>
+            <td class="border px-4 py-2">{{ product.description }}</td>
+            <td class="border px-4 py-2"></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
 
+  <Create :showModal="showModal"></Create>
 </template>
 
 <script>
-    import { defineComponent } from 'vue'
+import { defineComponent } from "vue";
 
-    import { 
-        PlusIcon, 
-        } from '@heroicons/vue/solid'
+import { PlusIcon } from "@heroicons/vue/solid";
 
-    import JetApplicationLogo from '@/Jetstream/ApplicationLogo.vue'
-    import JetSecondaryButton from '@/Jetstream/SecondaryButton.vue'
-    import JetDangerButton from '@/Jetstream/SecondaryButton.vue'
+import JetApplicationLogo from "@/Jetstream/ApplicationLogo.vue";
+import JetSecondaryButton from "@/Jetstream/SecondaryButton.vue";
+import JetDangerButton from "@/Jetstream/SecondaryButton.vue";
 
-    import Create from '@/Pages/Products/Create'
+import Create from "@/Pages/Products/Create";
 
-    export default defineComponent({
-        components: {
-            JetApplicationLogo,
-            JetSecondaryButton,
-            JetDangerButton,
-            PlusIcon,
-            Create,
-        },
+export default defineComponent({
+  components: {
+    JetApplicationLogo,
+    JetSecondaryButton,
+    JetDangerButton,
+    PlusIcon,
+    Create,
+  },
 
-        data(){
-            return {
-                showModal: false,
-            }
-        },
+  data() {
+    return {
+      showModal: false,
+    };
+  },
 
-        methods:{
-            modal(){
-                this.showModal = true;
-            }
-        },
+  methods: {
+    modal() {
+      this.showModal = true;
+    },
+  },
 
-        props: ['products'],
-    })
+  props: ["products"],
+});
 </script>
