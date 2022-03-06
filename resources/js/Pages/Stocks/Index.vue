07dbdb1c2aa0a0c4486a6db3640a4023ab8cf9c5
aridsonf <filhoarison@gmail.com>
2022-03-06 11:39:33 -0300

    style: link buttons

--- resources/js/Pages/Stocks/Index.vue
+++ resources/js/Pages/Stocks/Index.vue
@@ -1,9 +1,34 @@
 <template>
   <app-layout title="Dashboard">
     <template #header>
-      <h2 class="font-semibold flex text-xl text-gray-800 leading-tight">
-        <CollectionIcon class="h-5 w-5 text-dark-500" />&nbsp; Stocks
-      </h2>
+      <div class="flex justify-between">
+        <div class="col p-2">
+          <h2 class="font-semibold flex text-xl text-gray-800 leading-tight">
+            <CollectionIcon class="h-5 w-5 text-dark-500" />&nbsp; Stocks
+          </h2>
+        </div>
+
+        <div class="col text-right">
+          <a
+            href="/dashboard"
+            class="
+              inline-flex
+              items-center
+              bg-blue-500
+              hover:bg-blue-400
+              text-white
+              font-bold
+              py-2
+              px-4
+              border-b-4 border-blue-700
+              hover:border-blue-500
+              rounded-full
+            "
+          >
+            <HomeIcon class="h-5 w-5" />
+          </a>
+        </div>
+      </div>
     </template>
 
     <div class="py-12">
@@ -20,13 +45,14 @@
 import { defineComponent } from "vue";
 import AppLayout from "@/Layouts/AppLayout.vue";
 import List from "@/Pages/Stocks/List.vue";
-import { CollectionIcon } from "@heroicons/vue/solid";
+import { CollectionIcon, HomeIcon } from "@heroicons/vue/solid";
 
 export default defineComponent({
   components: {
     AppLayout,
     List,
     CollectionIcon,
+    HomeIcon,
   },
 
   props: ["stocks"],
